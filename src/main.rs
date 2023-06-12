75f8570240d643978b1238d4c972a611f5d8a2ef
AlexanderMaxRanabel <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-06-12 13:48:38 +0300

    Added more text

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![no_std]
 #![no_main]
 use core::panic::PanicInfo;
-static HELLO: &[u8] = b"Hello Calcadite!";
+static HELLO: &[u8] = b"Welcome to Calcadite Operating System";
+static WORLD: &[u8] = b"Version 0.0.1 Pre-Alpha";
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     let vga_buffer = 0xb8000 as *mut u8;
@@ -9,6 +10,13 @@ pub extern "C" fn _start() -> ! {
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    for (i, &byte) in WORLD.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize *  2) = byte;
             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
         }
     }
