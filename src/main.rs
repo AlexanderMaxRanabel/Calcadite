0955f038d6eab1f8ec922862e1acbeae9d000722
AlexanderMaxRanabel <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-06-14 10:09:21 +0300

    Added VGA Buffer Capabilities

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 #![no_std]
 #![no_main]
+
+mod vga_buffer;
+
 use core::panic::PanicInfo;
-static HELLO: &[u8] = b"Welcome to Calcadite Operating System";
-static WORLD: &[u8] = b"Version 0.0.1 Pre-Alpha";
+//static HELLO: &[u8] = b"Welcome to Calcadite Operating System";
+//static WORLD: &[u8] = b"Version 0.0.1 Pre-Alpha";
+//noinspection ALL
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    /*let vga_buffer = 0xb8000 as *mut u8;
 
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
@@ -17,9 +21,11 @@ pub extern "C" fn _start() -> ! {
     for (i, &byte) in WORLD.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize *  2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xd;
         }
-    }
+    }  <- The Old Way*/
+
+    vga_buffer::print();
     loop {}
 }
 
